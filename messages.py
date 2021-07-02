3e1bbcdd997f958f8523e4c555d7aad43234389d
Desarrollo Eiffage <fgarcia@energia.eiffage.es>
2021-07-02 09:40:27 +0200

    Initial commit

---
+++ messages.py
@@ -0,0 +1,66 @@
+'''
+AWS SQS. 
+
+
+'''
+import boto3
+import os
+
+os.environ['AWS_PROFILE'] = "profile1"
+
+sqs = boto3.client('sqs', region_name='us-east-2')
+
+queue_url = 'https://sqs.us-east-2.amazonaws.com/338433442274/cola1'
+
+
+def send_message(mensaje):
+    # Send message to the queue
+    response = sqs.send_message(
+        QueueUrl=queue_url, 
+        DelaySeconds=10,
+        MessageAttributes={
+        },
+        MessageBody=(
+            format(mensaje)
+        )
+    )
+
+    if response:
+        print(response['MessageId'])
+        return response['MessageId']
+
+    else:
+        return False
+
+
+def receive_message():
+    try:
+        # Receive message from SQS queue
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'SendTimeStamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+
+        message = response['Messages'][0]
+        print("---\n{}\n---".format(message['Body']))
+
+        '''
+        receipt_handle = message['ReceiptHandle']
+        # Delete received message from queue
+        sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+        )
+        '''
+        print('received and deleted message: %s' % message)
+    except:
+        message ="No pending messages"
+    return message
---
+++ server.py
@@ -0,0 +1,40 @@
+
+import flask
+from flask import request, jsonify
+
+import messages as msg
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Testing site</h1><p>AWS SQS demo</p>"
+
+@app.route('/api/v1/messages', methods=['GET'])
+def api_get_messages():
+    messages = msg.receive_message()
+    print (messages)
+    return jsonify(messages)
+
+@app.route('/api/v1/send/', methods=['POST'])
+def api_create_mensaje():
+    json = request.get_json(force=True)
+
+    if json.get('message') is None:
+        return jsonify({'message': 'Bad request'}), 400
+    #if json.get('data') is None:
+    #    return jsonify({'message': 'Bad request'}), 400
+
+    #user = User.create(json['username'])
+    mensaje = json['message']
+    #dato = json['data']
+    #print ("{} -- {}".format(mensaje,dato))
+
+    res = msg.send_message(mensaje)
+    
+    return jsonify({'res': mensaje })
+
+
+app.run()
